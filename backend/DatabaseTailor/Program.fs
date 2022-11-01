a824da449a28d19ec7177903244b395bb3d904f7
Christian Sheridan <christiansheridan@outlook.com>
2022-11-01 16:34:29 -0500

    Add backend project

---
+++ backend/DatabaseTailor/Program.fs
@@ -0,0 +1,7 @@
+﻿open Falco
+open Falco.Routing
+open Falco.HostBuilder
+
+let configuration = configuration [||] { add_env }
+
+webHost [||] { endpoints [ get "/ping" (Response.ofPlainText "pong!") ] }
