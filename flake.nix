991bf3d40e963130852a29fdb71e62d9cac2b1b2
Midka <me@midka.dev>
2022-11-01 17:58:46 +0200

    add: nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" ]
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        rec
+        {
+          devShell = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              dotnet-sdk
+              nodejs-18_x
+              nodePackages.npm
+            ];
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
+        });
+}
